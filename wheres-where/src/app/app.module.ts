6c62fadbe14195c07fda4ecfc43ab387886d2ae0
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-19 17:11:12 -0600

    Add missing component declarations in AppModule

--- wheres-where/src/app/app.module.ts
+++ wheres-where/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ResultsComponent } from './results/results.component';
+import { GameSettingsComponent } from './game-settings/game-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    LeaderboardComponent,
+    ResultsComponent,
+    GameSettingsComponent
   ],
   imports: [
     BrowserModule,
