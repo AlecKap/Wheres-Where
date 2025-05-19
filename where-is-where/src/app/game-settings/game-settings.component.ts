2d378d32ff82aed2f78fe7e13d5de450be8fd8f5
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-19 14:54:37 -0600

    Add GameSettingsComponent with HTML, CSS, and unit tests

---
+++ where-is-where/src/app/game-settings/game-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-settings',
+  imports: [],
+  templateUrl: './game-settings.component.html',
+  styleUrl: './game-settings.component.css'
+})
+export class GameSettingsComponent {
+
+}
