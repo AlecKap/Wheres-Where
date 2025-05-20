5a0e4776d53e015df4881a2afd6c14f9415761a6
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-19 21:36:20 -0600

    Add FlagService to fetch country flags from external API

---
+++ wheres-where/src/app/services/flag.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FlagResponse } from '../models/country.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FlagService {
+  private baseUrl = 'https://restcountries.com/v3.1/name';
+
+  constructor(private http: HttpClient) { }
+  
+  async getCountryFlag(countryName: string) {
+    let data = await this.http.get<FlagResponse[]>(`${this.baseUrl}/${countryName}?fields=flags`).toPromise();
+    console.log(data);
+    return data;
+  }
+}
