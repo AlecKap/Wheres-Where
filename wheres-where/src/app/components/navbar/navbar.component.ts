d3ffb848c43fda4e1047bab7019215511c961eb3
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-19 23:01:06 -0600

    Refactor navbar component styles and structure; update button styles and add responsive design

--- wheres-where/src/app/components/navbar/navbar.component.ts
+++ wheres-where/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) { }
 
+  isHomePage(): boolean {
+    return this.router.url === '/' || this.router.url === '/home';
+  }
+
+  isResultsPage(): boolean {
+    return this.router.url === '/results';
+  }
+
+  isLeaderboardPage(): boolean { 
+    return this.router.url === '/leaderboard';
+  }
 }
