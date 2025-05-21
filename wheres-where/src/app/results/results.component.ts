1aafb716059292a0be6f9b2e57ce9983b0891032
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-21 09:28:35 -0600

    Implement ResultsComponent with game results handling and navigation

--- wheres-where/src/app/results/results.component.ts
+++ wheres-where/src/app/results/results.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameResults } from '../models/game-results.interface';
 
 @Component({
   selector: 'app-results',
@@ -6,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit {
+  gameResults: GameResults | null = null;
+  objectKeys = Object.keys;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const resultsJson = localStorage.getItem('gameResults');
+    if (resultsJson) {
+      this.gameResults = JSON.parse(resultsJson);
+    } else {
+      this.router.navigate(['']);
+    }
+  }
+
+  playAgain() {
+    localStorage.removeItem('gameResults');
+    this.router.navigate(['/game-settings']);
+  }
+
+  hasCountryScores(): boolean {
+   if (!this.gameResults || !this.gameResults.countryScores) {
+        return false;
+    }
+    return Object.keys(this.gameResults.countryScores).length > 0;
+  }
+
+  sortedCountryScores(): string[] {
+    if (!this.gameResults?.continentScores) return [];
+    return Object.keys(this.gameResults.continentScores).sort((a, b) => 
+    this.gameResults!.continentScores[b].percentage - 
+    this.gameResults!.continentScores[a].percentage
+  );
+  }
 }
