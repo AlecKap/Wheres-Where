52f6b44d9e6e081f30fa07fd21746542b253b4b9
Niloo74 <143820105+Niloo74@users.noreply.github.com>
2025-05-21 23:41:07 +0000

    updates to leaderboard ts

--- wheres-where/src/app/leaderboard/leaderboard.component.ts
+++ wheres-where/src/app/leaderboard/leaderboard.component.ts
@@ -11,8 +11,17 @@ export class LeaderboardComponent implements OnInit {
   scores: { name: string; score: number }[] = [];
 
   ngOnInit() {
-    const storedScores = localStorage.getItem('flagGameLeaderboard');
+    const storedScores = localStorage.getItem('leaderboard');
     this.scores = storedScores ? JSON.parse(storedScores) : [];
-    this.scores.sort((a, b) => b.score - a.score); // highest score first
+    this.scores.sort((a, b) => b.score - a.score); // highest first although already taken care of in game
   }
+
+  getMedal(index: number): string | null {
+  switch (index) {
+    case 0: return '🥇';
+    case 1: return '🥈';
+    case 2: return '🥉';
+    default: return null;
+  }
+}
 }
