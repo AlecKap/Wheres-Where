faa3f41263fe578028a8ed72b86b2fa3c0c50105
niloo74 <nzafari@terpmail.umd.edu>
2025-05-23 11:18:59 -0400

    Add sound effects and confetti for win/lose states; implement lobby music control

--- wheres-where/src/app/home/home.component.ts
+++ wheres-where/src/app/home/home.component.ts
@@ -10,5 +10,11 @@ import { SoundService } from '../services/sound.service';
 })
 export class HomeComponent {
  constructor(public soundService: SoundService) {}
+
+ngOnInit() {
+  this.soundService.stopLobbyMusic();
+  this.soundService.playWin(); // or .playLose()
+}
+
 }
 
